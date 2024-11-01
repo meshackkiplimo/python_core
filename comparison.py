9f82305a30ea21acf1d447b864676351dbf97e8a
meshack kimaiyo <meshackkimaiyo5@gmail.com>
2024-11-01 16:54:57 +0300

    comparison with input

--- comparison.py
+++ comparison.py
@@ -1,5 +1,10 @@
-temp = 50
+
+
+temp = float(input("enter the temperatres of the day:"))
 if temp >30:
     print("It's too hot!")
+elif temp ==23:
+    print("It's perfect!")
 else:
-    print("It's not too hot.")
+    print("It's too cold!")
+    
